2d26b711c3c026e27b18008e0bf5b4091ef391a5
Nicolas Legrand <nicolas.legrand@gmail.com>
2023-05-25 13:35:03 +0200

    Can now comptute the median of a Vector

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,39 @@
 // sorted, the value in the middle position) and mode (the value that
 // occurs most often; a hash map will be helpful here) of the list.
 
-fn main() {
-    let mut v = [2,3,55,8,5,6,3,5,4,7,8,9,6,22,5,1,5,8,4,2,5,8,554,654,3,354,5,5,22,5,2,2365,53,2,2,2,5,35,33,35,35,5,5,1,4,7,5];
-    v.sort();
-    for i in v.iter() {
-        println!("{}", i)
+struct Median {
+    pos: usize,
+    value: i32,
+}
+
+fn median(v: Vec<i32>) -> Median {
+    let length = v.len();
+    let modulo = length % 2;
+    if modulo != 0 { //even
+        let median_pos = (length + 1) / 2 -1;
+        return Median {
+            pos: median_pos,
+            value: v[median_pos],
+        }
     }
+    else { //odd
+        let median_pos = length / 2 -1;
+        return Median {
+            pos: median_pos,
+            value: v[median_pos],
+        }
+    }
+}
+
+fn main() {
+    let mut v = vec![2i32,3,55,8,5,6,3,5,4,7,-1];
+    v.sort();
+    let mut pos = 0;
+    for i in v.iter() {
+        println!("{} -> {}", pos, i);
+        pos = pos + 1
+    }
+    println!("v is {} long", v.len());
+    let med = median(v);
+    println!("Median is in pos {} with value {}", med.pos, med.value)
 }
